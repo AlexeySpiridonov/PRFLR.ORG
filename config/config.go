58f578112fe9223e5d7b0f13739238306e8706ed
spall <forspall@gmail.com>
2017-01-09 21:10:01 +0700

    add db recreate

--- config/config.go
+++ config/config.go
@@ -17,7 +17,7 @@ const (
 	HTTPPort = ":8080"
 
 	CappedCollectionMaxByte = 50000000
-	CappedCollectionMaxDocs = 100000    
+	CappedCollectionMaxDocs = 100000
 
 	RegisterEmailFrom    = "robot@prflr.org"
 	RegisterEmailTo      = "info@prflr.org"
